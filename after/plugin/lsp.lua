db22b19a5e284fc886144e6d2a456e72eca2be5d
Artur <artur.schaefer32@gmail.com>
2022-12-26 16:10:12 +0100

    first commit

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,27 @@
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+
+lsp.ensure_installed({
+    'tsserver',
+    'eslint',
+    'sumneko_lua',
+    'rust_analyzer',
+})
+
+-- Fix Undefined global 'vim'
+lsp.configure('sumneko_lua', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
+lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true,
+})
---
+++ init.vim
@@ -0,0 +1,15 @@
+lua require('remap')
+lua require('plugins')
+set nu
+set rnu
+set guicursor=""
+set mouse=a
+set smartindent
+set tabstop=4
+set softtabstop=4
+set shiftwidth=4
+set expandtab
+set incsearch
+set scrolloff=8
+
+syntax on
