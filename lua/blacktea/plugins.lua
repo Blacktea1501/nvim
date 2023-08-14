ebf6727c610a8550c62b737c1ffbee80fff6906c
Blacktea1501 <artur.schaefer32@gmail.com>
2023-08-14 15:19:49 +0200

    Migrated from vim-airline to lua line

--- lua/blacktea/plugins.lua
+++ lua/blacktea/plugins.lua
@@ -5,43 +5,47 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
     -- Colorsceme
     use 'navarasu/onedark.nvim'
-    use 'vim-airline/vim-airline'
+    -- statusline
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
     -- commentary
     use 'tpope/vim-commentary'
-	-- treesitter
-    use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-	use 'nvim-treesitter/playground'
+    -- treesitter
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use 'nvim-treesitter/playground'
     use 'theprimeagen/harpoon'
     use 'mbbill/undotree'
-	-- fuzzy finder
+    -- fuzzy finder
     use {
- 		 'nvim-telescope/telescope.nvim', tag = '0.1.0',
-		-- or                            , branch = '0.1.x',
-  		requires = { {'nvim-lua/plenary.nvim'} }
-	    }
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
     -- LSP
     use 'hrsh7th/nvim-compe'
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-buffer'},
-			{'hrsh7th/cmp-path'},
-			{'saadparwaiz1/cmp_luasnip'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'hrsh7th/cmp-nvim-lua'},
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
 
-			-- Snippets
-			{'L3MON4D3/LuaSnip'},
-			{'rafamadriz/friendly-snippets'},
-		}
-	}
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
+    }
     use 'tpope/vim-repeat'
     -- replace with registers
     use 'vim-scripts/ReplaceWithRegister'
